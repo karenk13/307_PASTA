e26ce05e39895908bb7deb213423f46d0aac12a7
Jon T Scott <jonscott20@gmail.com>
2017-05-01 15:37:48 -0700

    656699 Added the Basic Driver

---
+++ Pasta.java
@@ -0,0 +1,12 @@
+// The point of entry for the PASTA application
+// This is basically the driver
+public class Pasta
+{
+   public static void main(String[] args)
+   {
+      Assignment assignment = new Assignment(new Date(2017,04,26,15,45,30));
+      assignment.workOnAssignment();
+      assignment.pauseWorkOnAssignment();
+      System.out.println("Time Spent: " + assignment.timeSpentOnAssignment());
+   }
+}
